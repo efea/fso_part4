906205e57b2124794f3dd0ea1e0e9cf3ad5fd748
efea <astarlioglu@outlook.com>
2023-03-26 20:27:20 +0300

    added 4_4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0){
+    return 0
+  }
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  const initialValue = 0
+  return blogs.reduce(reducer, initialValue)
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
