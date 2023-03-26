0a65edaeef64bf720573854e28c66f1405ca7fc3
efea <astarlioglu@outlook.com>
2023-03-26 22:05:52 +0300

    added 4_6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -37,8 +37,47 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  if(blogs.length === 0){
+    return null
+  }
+  var uniques = new Array()
+  var y = new Array()
+  for (let i = 0; i < blogs.length; i++) {
+    if(!uniques.includes(blogs[i].author)){
+      uniques.push(blogs[i].author)
+    }
+  }
+  for(let i = 0; i < uniques.length; i++){
+    y[i] = 0
+  }
+  for (let i = 0; i < uniques.length; i++) {
+    for (let k = 0; k < blogs.length; k++) {
+      if(uniques[i] === blogs[k].author){
+        y[i] = y[i] + 1
+      }
+    }
+  }
+
+  var maxi = 0
+  var indexOfMax = 0
+  for (let i = 0; i < y.length; i++) {
+    if(y[i] >= maxi){
+      maxi = y[i]
+      indexOfMax = i
+    }
+  }
+  const xx = uniques[indexOfMax]
+  return {
+    author: xx,
+    blogs: maxi
+  }
+
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
